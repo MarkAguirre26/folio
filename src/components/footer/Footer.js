dcfa23a0134a252feaf390185948bc2744e8c402
Koi git <96707504+koiy7010@users.noreply.github.com>
2025-01-04 16:30:11 +0800

    removed extras

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,10 +8,7 @@ export default function Footer() {
   const {isDark} = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
-        </p>
+      <div className="footer-div">       
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a
